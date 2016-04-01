bbf65bc5bc1396204af7a611cffbe80106ec5d83
sreeni <sreeni.san1312@gmail.com>
2016-03-31 20:42:36 -0500

    chore - reducers and the index file

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,22 @@
+export default function () {
+
+  return [
+    {
+      title: 'Javascript',
+      pages: 101
+    },
+    {
+      title: 'React',
+      pages: 200
+    },
+    {
+      title: 'Redux',
+      pages: 150
+    },
+    {
+      title: 'Flux',
+      pages: 201
+    }
+  ]
+
+}
